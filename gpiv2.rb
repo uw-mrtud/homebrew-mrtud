d68ec730d11e48ae2d376a52084ec44b618d7411
John Christensen <jechristens3@wisc.edu>
2025-04-28 10:54:12 -0500

    remove sha256

--- gpiv2.rb
+++ gpiv2.rb
@@ -3,7 +3,7 @@ class Gpiv2 < Formula
     homepage "https://github.com/gpilab/gpi-v2"
     url "https://github.com/gpilab/gpi-v2.git",branch: "main"
        # revision: "ab5406b3640f4c9d9311fc759284d6ce01fe0b23"
-    sha256 "7f1578dac8f0a8d620f39352924f0085932404141516c4780a9bee7efa4c8b0a"
+    #sha256 "7f1578dac8f0a8d620f39352924f0085932404141516c4780a9bee7efa4c8b0a"
   
     depends_on "rust" => :build
   
