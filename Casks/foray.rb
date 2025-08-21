3d2cf99881be4c4f46681439be96175907e477d6
John Christensen <jechristens3@wisc.edu>
2025-08-21 13:34:46 -0500

    fix zip

--- Casks/foray.rb
+++ Casks/foray.rb
@@ -1,6 +1,6 @@
 cask "foray" do
   version "0.1.5"
-  sha256 "093d5e9b758eb8c16d321baca7ed21eae46322bafa1a2e61302509b4d3231e08"
+  sha256 "f43ba0ed1821bf7c7ca0356f87291a9e6ca86cee1c1ec78e04ce543648501b55"
 
   url "https://github.com/uw-mrtud/foray/releases/download/v0.1.5/Foray.zip"
   name "Foray"
