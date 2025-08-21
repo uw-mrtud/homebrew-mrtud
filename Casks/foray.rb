14dc8290d1b034cc9bdc07be96531c39586c4684
John Christensen <jechristens3@wisc.edu>
2025-08-21 13:30:31 -0500

    v0.1.5

--- Casks/foray.rb
+++ Casks/foray.rb
@@ -1,8 +1,8 @@
 cask "foray" do
-  version "0.1.4"
+  version "0.1.5"
   sha256 "093d5e9b758eb8c16d321baca7ed21eae46322bafa1a2e61302509b4d3231e08"
 
-  url "https://github.com/uw-mrtud/foray/releases/download/v0.1.4/Foray.zip"
+  url "https://github.com/uw-mrtud/foray/releases/download/v0.1.5/Foray.zip"
   name "Foray"
   desc "Interactive signal processing toolkit"
   homepage "https://github.com/gpilab/foray/"
