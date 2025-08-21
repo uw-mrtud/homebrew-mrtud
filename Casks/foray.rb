24a3a1924fccb45c484e51f5419f043a5d90fa4a
John Christensen <jechristens3@wisc.edu>
2025-08-21 12:34:16 -0500

    fix: update version field to v0.1.3

--- Casks/foray.rb
+++ Casks/foray.rb
@@ -1,5 +1,5 @@
 cask "foray" do
-  version "0.1.2"
+  version "0.1.3"
   sha256 "a0a602e814fe0b003d78f6e843bde29d288ed1cc0fd1912bf730f65504efb3ba"
 
   url "https://github.com/uw-mrtud/foray/releases/download/v0.1.3/Foray.zip"
