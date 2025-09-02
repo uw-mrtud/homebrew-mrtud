bbd128f1431869fd1c49fad9396fb03a75dfcf45
John Christensen <jechristens3@wisc.edu>
2025-09-02 12:04:32 -0500

    Fix url

--- Casks/foray.rb
+++ Casks/foray.rb
@@ -2,7 +2,7 @@ cask "foray" do
   version "0.1.7"
   sha256 "c60e337b41c472e2d8a86ff6460f9d025ed52e97be7e8d7ed15844b035ff3e50"
 
-  url "https://github.com/uw-mrtud/foray/releases/download/v{#version}/Foray-v{#version}.zip"
+  url "https://github.com/uw-mrtud/foray/releases/download/v#{version}/Foray-v#{version}.zip"
   name "Foray"
   desc "Interactive signal processing toolkit"
   homepage "https://github.com/gpilab/foray/"
