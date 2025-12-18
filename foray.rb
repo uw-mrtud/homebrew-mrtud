7e77e88f53a0e58428e0dfdb1bb40b9e7205f509
John Christensen <jechristens3@wisc.edu>
2025-12-18 14:44:18 -0600

    Bump version

--- foray.rb
+++ foray.rb
@@ -1,6 +1,6 @@
 class Foray < Formula
   desc "Interactive signal processing toolkit"
-  version "0.1.19"
+  version "0.1.20"
   homepage "https://github.com/uw-mrtud/foray"
   url "https://github.com/uw-mrtud/foray/archive/refs/tags/v#{version}.tar.gz"
   sha256 "55af51f254db8986733156ebfef5c790ea57485f13b3e686a71348b362de3a26"
