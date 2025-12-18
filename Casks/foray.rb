7403c4b71f3e4788fa6b6a30a198fc0b0a82fd2c
John Christensen <jechristens3@wisc.edu>
2025-12-18 11:38:45 -0600

    bump version

--- Casks/foray.rb
+++ Casks/foray.rb
@@ -1,5 +1,5 @@
 cask "foray" do
-  version "0.1.12"
+  version "0.1.13"
   sha256 "a1c0d0878d8a02dd1b07170e54e21aae09956e0253582b0ed07f16443e726521"
 
   url "https://github.com/uw-mrtud/foray/releases/download/v#{version}/Foray-v#{version}.zip"
