959b5ce2fb235bd180804ecb7dcfc2542f13d658
John Christensen <jechristens3@wisc.edu>
2025-12-18 14:38:40 -0600

    Add version

--- foray.rb
+++ foray.rb
@@ -1,5 +1,6 @@
 class Foray < Formula
-  desc ""
+  desc "Interactive signal processing toolkit"
+  version "0.1.19"
   homepage "https://github.com/uw-mrtud/foray"
   url "https://github.com/uw-mrtud/foray/archive/refs/tags/v#{version}.tar.gz"
   sha256 "b26e66fd6574d7547e8cc73afcf5ddcb37603063df4685bfdc30ab2efbb35f48"
